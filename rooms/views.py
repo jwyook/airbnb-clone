562d1cdffe837ea56c5f1eaa03934f603933065d
Yook JIWON <jwyook27@naver.com>
2022-01-10 17:32:59 +0900

    #13 SearchView Setup

--- rooms/views.py
+++ rooms/views.py
@@ -1,33 +1,7 @@
-#  For Function Based View
-# from math import ceil
-# from django.shortcuts import render, redirect
-# from django.core.paginator import EmptyPage, Paginator
-# from . import models
-
-
 # For Class Based View
 from django.views.generic import ListView, DetailView
-from django.http import Http404
 from django.shortcuts import render
 from . import models
-
-# Create your views here.
-
-#  Function based view
-# def all_rooms(request):
-#     page = request.GET.get("page", 1)
-#     room_list = models.Room.objects.all()
-#     paginator = Paginator(room_list, 10, orphans=5)
-
-#     try:
-#         rooms = paginator.page(int(page))
-#         return render(
-#             request,
-#             "rooms/home.html",
-#             context={"page": rooms},
-#         )
-#     except EmptyPage:
-#         return redirect("/")
 
 
 class HomeView(ListView):
@@ -40,15 +14,13 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-# def room_detail(request, pk):
-#     try:
-#         room = models.Room.objects.get(pk=pk)
-#         return render(request, "rooms/detail.html", {"room": room})
-#     except models.Room.DoesNotExist:
-#         raise Http404()
-
-
 class RoomDetail(DetailView):
     """RoomDetail Definition"""
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
