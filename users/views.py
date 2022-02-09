b7acce11f5241bf7a72748c5417ac3e9f542390f
Yook JIWON <jwyook27@naver.com>
2022-02-09 19:19:03 +0900

    #18.1 Kakao Profile Photo

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
 from . import forms, models
 
 # Create your views here.
@@ -181,6 +182,13 @@ def kakao_callback(request):
             )
             user.set_unusable_password()
             user.save()
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
+
+                user.avatar.save(
+                    f"{nickname}-avatar", ContentFile(photo_request.content())
+                )
+                user.save()
         login(request, user)
         return redirect(reverse("core:home"))
     except KakaoException:
